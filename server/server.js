17c268d1f438292df248bdf671be344a01d89f51
Cray Smith <cmsmith004@gmail.com>
2022-01-31 10:00:34 -0700

    Heroku bug testing

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
 const routes = require("./routes");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,11 +12,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.use(routes);
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-
-app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
