d5a36c0817c6322e57665ff14736899f8db342ad
Cray Smith <cmsmith004@gmail.com>
2022-02-03 01:55:58 -0700

    Trying to build mutations in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,22 +2,50 @@ const { deleteBook } = require("../controllers/user-controller");
 const { User } = require("../models");
 const { AuthenticationError } = require("../utils/auth");
 
-// const { signToken } = require("../utils/auth");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate("books");
+        const UserData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return UserData;
       }
+      throw new AuthenticationError("Please log in first!");
     },
   },
   Mutation: {
-    createUser: async (parent, { userName, email, password }) => {
+    addUser: async (parent, args) => {
       // Create and return the new School object
-      return await User.create({ userName, email, password });
+      const user = await User.create(args);
+      const Token = signToken(user);
+
+      return { token, user };
     },
-    deleteBook: async (parent, { bookId }, context) => {
+    login: async (parent, { email, password }) => {
+      const user = await User.findone({ email });
+      if (!user) {
+        throw new AuthenticationError("Wrong username or password");
+      }
+      const correctPw = await user.isCorrectPassword(pasword);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Wrong username or password");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { input }, { user }) => {
+      if (user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: input } },
+          { new: true, runValidators: true }
+        );
+      }
       await Book.findOneAndDelete({ savedBooks, bookId: context.user.bookId });
       return { success: true, message: "got'em" };
     },
