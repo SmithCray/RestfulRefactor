49bc85dd84269b224005809ec056e66459e77195
Cray Smith <cmsmith004@gmail.com>
2022-02-03 13:50:15 -0700

    testing server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 // Apollo server
 const server = new ApolloServer({
@@ -24,9 +24,9 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
 
 app.use(routes);
 
