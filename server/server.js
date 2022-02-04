c64a822d3ee30c1f5c91779e82a853c214ad981a
Cray Smith <cmsmith004@gmail.com>
2022-02-03 22:19:50 -0700

    testing server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 // Apollo server
 const server = new ApolloServer({
