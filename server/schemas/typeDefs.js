ac044f6e58c92835e45299d42edecd4711056473
Cray Smith <cmsmith004@gmail.com>
2022-02-03 21:40:30 -0700

    Working on typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -15,7 +15,7 @@ const typeDefs = gql`
     link: String
   }
   type User {
-    id: ID
+    _id: ID
     username: String
     email: String
     bookCount: Int
@@ -36,6 +36,7 @@ const typeDefs = gql`
   type Query {
     me: User
   }
+
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
