916115e37ccfdc5ed6eaf8f01b8f7dd965b688ab
Cray Smith <cmsmith004@gmail.com>
2022-02-04 00:08:18 -0700

    testing gitignore

---
+++ client/build/precache-manifest.f3d63ec4cab997c7fb70eb1c96c218b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029e6a988a722593d381740f6530dd9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f4c58b1838074e90036",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "35f9f706b87abe240c41",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "8f4c58b1838074e90036",
+    "url": "/static/js/2.0b4b000f.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.0b4b000f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f9f706b87abe240c41",
+    "url": "/static/js/main.9e7dbc10.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
